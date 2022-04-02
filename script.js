f02d1f5af5189848c9dad4aaa33bbf466a735f02
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-01 23:09:27 -0300

    agora conteúdo do portfolio vem totalmente do JS e não mais é descrito estaticamente no html

--- script.js
+++ script.js
@@ -1,33 +1,129 @@
 const projects = [
     {
-        title: 'dt-money',
-        description: 'Aplicativo para acompanhamento de despesas e receitas',
-        stack: ['HTML', 'CSS'],
+        title: 'DT money',
+        imgName: 'dtmoney.png',
+        description: 'Esta é uma aplicação desenvolvida para ajudar as pessoas a monitorarem seus gastos e receitas, separados por categoria.',
+        stack: ['react'],
         siteUrl: '',
         codeUrl: 'https://github.com/matheus-ferreira1/DT-money'
     },
     {
-        title: 'profitchart',
-        description: 'Projetos desenvolvidos atuando como freelancer, para a plataforma de operações em bolsa ProfitChart',
-        stack: ['HTML', 'CSS'],
-        siteUrl: '',
-        codeUrl: 'https://github.com/matheus-ferreira1/DT-money'
+        title: 'Landing page',
+        imgName: 'land.png',
+        description: 'Esta é uma landing page para um site de venda de serviços holísticos. A mesma contém carrossel e responsividade.',
+        stack: ['html', 'css', 'js'],
+        siteUrl: 'https://matheus-ferreira1.github.io/landing-page/',
+        codeUrl: 'https://github.com/matheus-ferreira1/landing-page'
     },
     {
-        title: 'dt-money',
-        description: 'Aplicativo para acompanhamento de despesas e receitas',
-        stack: ['HTML', 'CSS'],
-        siteUrl: '',
-        codeUrl: 'https://github.com/matheus-ferreira1/DT-money'
+        title: 'Calculadora',
+        imgName: 'calc.png',
+        description: 'Calculadora simples totalmente funcional, semelhante as calculadoras disponíveis em dispositivos móveis e desktop',
+        stack: ['html', 'css', 'js'],
+        siteUrl: 'https://matheus-ferreira1.github.io/Calculadora-Basica/',
+        codeUrl: 'https://github.com/matheus-ferreira1/Calculadora-Basica'
     },
     {
-        title: 'dt-money',
-        description: 'Aplicativo para acompanhamento de despesas e receitas',
-        stack: ['HTML', 'CSS'],
-        siteUrl: '',
-        codeUrl: 'https://github.com/matheus-ferreira1/DT-money'
+        title: 'Clone home Spotify',
+        imgName: 'spotify.png',
+        description: 'Clone da landing page do Spotify. O projeto possui um carrossel funcional Projeto feito utilizando o framework Bootstrap 4.',
+        stack: ['html', 'css'],
+        siteUrl: 'https://matheus-ferreira1.github.io/Pagina-Spotify-Clone/',
+        codeUrl: 'https://github.com/matheus-ferreira1/Pagina-Spotify-Clone'
+    },
+    {
+        title: 'Player de música',
+        imgName: 'player.png',
+        description: 'Music player totalmente funcional, com 5 faixas já embarcadas. Opções de auto replay e mute inclusas.',
+        stack: ['js'],
+        siteUrl: 'https://matheus-ferreira1.github.io/music-player-JavaScript/',
+        codeUrl: 'https://github.com/matheus-ferreira1/music-player-JavaScript'
+    },
+    {
+        title: 'Game FPS Trainer',
+        imgName: 'fps.png',
+        description: 'Game para eliminar alvos randomicos que aparecem, baseados na dificuldade e time selecionados no início.',
+        stack: ['js'],
+        siteUrl: 'https://matheus-ferreira1.github.io/game-FPS-trainer/',
+        codeUrl: 'https://github.com/matheus-ferreira1/game-FPS-trainer'
+    },
+    {
+        title: 'Calculadora IMC',
+        imgName: 'imc.png',
+        description: 'Calculadora de índice de massa corporal, semelhante as calculadoras disponíveis na web, com uma tabela colorida de referência. O resultado é dado com cor de fundo igual a da tabela.',
+        stack: ['js'],
+        siteUrl: 'https://matheus-ferreira1.github.io/Calculadora-IMC/',
+        codeUrl: 'https://github.com/matheus-ferreira1/Calculadora-IMC'
+    },
+    {
+        title: 'Calculadora juros compostos',
+        imgName: 'juros.png',
+        description: 'Este é um projeto com a finalidade de criar uma calculadora de juros simples e compostos, desenvolvida inteiramente com HTML e CSS, e tem sua lógica em JavaScript.',
+        stack: ['js'],
+        siteUrl: 'https://matheus-ferreira1.github.io/calculadora-juros-compostos/',
+        codeUrl: 'https://github.com/matheus-ferreira1/calculadora-juros-compostos'
+    },
+    {
+        title: 'Profitchart (mercado financeiro)',
+        imgName: 'stock.jpg',
+        description: 'Projetos desenvolvidos atuando como freelancer, para a plataforma de operações em bolsa ProfitChart (Nelogica)',
+        stack: ['others'],
+        siteUrl: 'https://github.com/matheus-ferreira1/ProfitChart-Mercado-Financeiro',
+        codeUrl: 'https://github.com/matheus-ferreira1/ProfitChart-Mercado-Financeiro'
     },
 ]
+
+const portWrapper = document.querySelector('.port-wrapper');
+
+function renderPage() {
+    portWrapper.innerHTML = '';
+
+    for (i = 0; i < projects.length; i++) {
+        createItems(projects[i])
+    }
+}
+renderPage();
+
+
+function createItems(projects) {
+
+    const cardDiv = document.createElement('div');
+    const cardContent = document.createElement('div');
+    const img = document.createElement('img');
+    const h4 = document.createElement('h4');
+    const paragraph = document.createElement('p');
+    const linkDiv = document.createElement('div');
+    const linkSite = document.createElement('a');
+    const linkCode = document.createElement('a');
+
+    cardDiv.className = `card ${projects.stack.toString().replaceAll(',', ' ')}`;
+    cardContent.className = 'card-content';
+    linkDiv.className = 'links';
+
+    img.src = `/img/${projects.imgName}`;
+    h4.innerHTML = projects.title;
+    paragraph.innerHTML = projects.description;
+    linkSite.setAttribute('href', projects.siteUrl);
+    linkSite.setAttribute('target', '_blank');
+    linkSite.textContent = 'Site';
+    linkCode.setAttribute('href', projects.codeUrl);
+    linkCode.setAttribute('target', '_blank');
+    linkCode.textContent = 'Código';
+
+    cardDiv.appendChild(cardContent);
+    cardContent.appendChild(img);
+    cardContent.appendChild(h4);
+    cardContent.appendChild(paragraph);
+    cardContent.appendChild(linkDiv);
+    linkDiv.appendChild(linkSite);
+    linkDiv.appendChild(linkCode);
+
+    portWrapper.appendChild(cardDiv);
+
+
+
+
+}
 
 
 cards = document.querySelectorAll('.card')
